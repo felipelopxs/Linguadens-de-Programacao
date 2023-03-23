2c626588abc36ea76f4225574544b4ec847555d9
Felipe Guilherme <108408157+felipelopxs@users.noreply.github.com>
2023-03-22 23:10:24 -0300

    Update Acai.java

--- Acai_Pedido/Acai.java
+++ Acai_Pedido/Acai.java
@@ -1,1 +1,35 @@
 
+package br.com.newton.exercicioacai;
+
+public class Acai {
+    private int tamanho;
+    private double valor;
+    private boolean possuiAcrescimo;
+
+    public Acai() {
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public boolean isPossuiAcrescimo() {
+        return possuiAcrescimo;
+    }
+
+    public void setPossuiAcrescimo(boolean possuiAcrescimo) {
+        this.possuiAcrescimo = possuiAcrescimo;
+    }
+
+    public Double Calculavalor(boolean possuiAcrescimo){
+        if(possuiAcrescimo){
+            double valor = getValor()+2;
+            setValor(valor);
+        }
+        return this.valor;
+    }
+}
